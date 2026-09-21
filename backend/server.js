b3fab3ea6c705ba9dbdf90ae919cb96f32be9269
Sibtain Raza <khanbhai14063@gmail.com>
2026-09-21 16:52:55 +0530

    Fix cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://hire-up-placement-portal.vercel.app/"
+      "https://hire-up-placement-portal.vercel.app"
     ],
     credentials: true,
   })
